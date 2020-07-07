27846f26497439bcebb5c1f09389fcec757f5793
Alexander Pavlenko <pava@bk.ru>
2020-07-07 23:44:26 +0700

    Test added

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## Краткое описание
 
 1. Cоздан gradle проект
-2. Создан тестовый класс: 
-     - в поле телефонного номера вводится неправильное значение 
-3. Настроен CI
+2. Создан тестовый класс.
+3. Создан метод для получение требуемой даты.  
+4. Настроен CI
 
-## Project 
+## Project on Appveyor [![Build status](https://ci.appveyor.com/api/projects/status/52bjo88pmc2vv4wl/branch/master?svg=true)](https://ci.appveyor.com/project/pava-14/aqa2-2-2/branch/master)
---
+++ src/test/java/ru/netology/carddelivery/CardDeliveryFormTest.java
@@ -0,0 +1,52 @@
+package ru.netology.carddelivery;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryFormTest {
+    private int daysDiff = 7;
+    private String cityDelivery = "Новосибирск";
+
+    private String GetOrderDay(int daysDiff) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd");
+        return dateFormat.format(LocalDateTime.now().plusDays(daysDiff));
+    }
+
+    private String GetOrderDate(int daysDiff) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return dateFormat.format(LocalDateTime.now().plusDays(daysDiff));
+    }
+
+    @Test
+    public void shouldCreditCardDeliveryOrder() {
+
+        open("http://localhost:9999");
+
+        String dayOrder = GetOrderDay(daysDiff);
+        String dateOrder = GetOrderDate(daysDiff);
+
+        SelenideElement body = $("body");
+        SelenideElement element = $("form");
+        element.$("[data-test-id=city] input").setValue("Но");
+        $(byText(cityDelivery)).click();
+        element.$("[data-test-id=date] input").click();
+        $(byText(dayOrder)).click();
+        element.$("[data-test-id=name] input").setValue("Иванов Петр Петрович");
+        element.$("[data-test-id=phone] input").setValue("+79099099090");
+        element.$("[data-test-id=agreement]").click();
+        element.$$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+        $(byText("Встреча успешно забронирована на")).shouldBe(visible);
+        $(byText(dateOrder)).shouldBe(visible);
+    }
+}
